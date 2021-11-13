eb1dbd5d65e182031d7b9ca57ba3f967e994eccd
the5teven <64325583+the5teven@users.noreply.github.com>
2021-11-12 21:31:55 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from AESteve import AES
 # First create an instance of the AES. Remember to enter your key in hex!"
 aes = AES(key="54 68 61 74 73 20 6D 79 20 4B 75 6E 67 20 46 75")
-#Playing text must be in bytes. Use .encode() or use a byte string
+# Plain text must be in bytes. Use .encode() or use a byte string
 msg = b"hello world"
 #or
 msg = "Hello World! 🌎".encode() # hey its unicode!
